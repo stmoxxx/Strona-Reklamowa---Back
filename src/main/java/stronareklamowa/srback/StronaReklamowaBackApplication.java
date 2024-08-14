9e330859f40073fb3bd7cb07e40d57d8d5a7fb5f
Maksim Vlasov <maximality0320@gmail.com>
2024-08-14 15:07:41 +0200

    Start of the project

---
+++ src/main/java/stronareklamowa/srback/StronaReklamowaBackApplication.java
@@ -0,0 +1,13 @@
+package stronareklamowa.srback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaReklamowaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaReklamowaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stronareklamowa/srback/StronaReklamowaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package stronareklamowa.srback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StronaReklamowaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
